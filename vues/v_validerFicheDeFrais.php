4ccf131508ebf629dc90ef2c37e12f28c0a0550d
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2020-11-03 14:55:19 +0100

    Finalisation du tableau element forfaitisés

--- vues/v_validerFicheDeFrais.php
+++ vues/v_validerFicheDeFrais.php
@@ -1,4 +1,9 @@
-<?php $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING); ?>
+<?php
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+if ($action == 'selectionnerMois') {
+    unset($_SESSION['date']);
+}
+?>
 <h2>Fiches de frais</h2>
 <div class="row">
     <div class="col-md-4">
@@ -18,12 +23,12 @@
                         if ($mois == $moisASelectionne) {
                             ?>
                             <option selected value="<?php echo $mois ?>">
-                                <?php echo $numAnnee . '/' . $numMois ?> </option>
+                            <?php echo $numAnnee . '/' . $numMois ?> </option>
                             <?php
                         } else {
                             ?>
                             <option value="<?php echo $mois ?>">
-                                <?php echo $numAnnee . '/' . $numMois ?> </option>
+                            <?php echo $numAnnee . '/' . $numMois ?> </option>
                             <?php
                         }
                     }
@@ -33,42 +38,43 @@
             <input id="okDate" type="submit" value="Valider" class="btn btn-success" 
                    role="button">           
         </form>
-        <form action="index.php?uc=validerFicheDeFrais&action=AffichageFicheFraisAndVisiteur" method="post" role="form">
-            <div class="form-group">
-                <label for="lstVisiteur" accesskey="n">Visiteur : </label>
-                <select id="lstVisiteur" name="lstVisiteur" class="form-control">
-                    <?php
-                    if (empty($_SESSION['date'])) {
-                        $date = str_replace('/', '', filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING));
-                        trim($date);                      
-                        $_SESSION['date'] = $date;
-                        $lesVisiteur = $pdo->getVisiteurFromMois($date);
-                        $selectedValue = $lesVisiteur[0];
-                        foreach ($lesVisiteur as $unVisiteur) {
-                            $idvisi = $unVisiteur['visiteur'];
-                            if ($selectedValue == $idvisi) {
-                                ?><option selected value="<?php echo $unVisiteur['visiteur'] ?>"><?php echo $unVisiteur['visiteur'] ?></option>               
-                            <?php } else { ?> <option value="<?php echo $idvisi ?>"><?php echo $idvisi ?></option> <?php
+<?php if ($action == 'selectionnerVisiteur' || $action == 'AffichageFicheFraisAndVisiteur') { ?>
+            <form action="index.php?uc=validerFicheDeFrais&action=AffichageFicheFraisAndVisiteur" method="post" role="form">
+                <div class="form-group">
+                    <label for="lstVisiteur" accesskey="n">Visiteur : </label>
+                    <select id="lstVisiteur" name="lstVisiteur" class="form-control">
+                        <?php
+                        if (empty($_SESSION['date'])) {
+                            $date = str_replace('/', '', filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING));
+                            trim($date);
+                            $_SESSION['date'] = $date;
+                            $lesVisiteur = $pdo->getVisiteurFromMois($date);
+                            $selectedValue = $lesVisiteur[0];
+                            foreach ($lesVisiteur as $unVisiteur) {
+                                $idvisi = $unVisiteur['visiteur'];
+                                if ($selectedValue == $idvisi) {
+                                    ?><option selected value="<?php echo $unVisiteur['visiteur'] ?>"><?php echo $unVisiteur['visiteur'] ?></option>               
+                                <?php } else { ?> <option value="<?php echo $idvisi ?>"><?php echo $idvisi ?></option> <?php
+                                }
+                            }
+                        } else {
+                            $lesVisiteur = $pdo->getVisiteurFromMois($_SESSION['date']);
+                            $selectedValue = $lesVisiteur[0];
+                            foreach ($lesVisiteur as $unVisiteur) {
+                                $idvisi = $unVisiteur['visiteur'];
+                                if ($selectedValue == $idvisi) {
+                                    ?><option selected value="<?php echo $unVisiteur['visiteur'] ?>"><?php echo $unVisiteur['visiteur'] ?></option>               
+                                <?php } else { ?> <option value="<?php echo $idvisi ?>"><?php echo $idvisi ?></option> <?php
+                                }
                             }
                         }
-                    } else {
-                        $lesVisiteur = $pdo->getVisiteurFromMois($_SESSION['date']);
-                        $selectedValue = $lesVisiteur[0];
-                        foreach ($lesVisiteur as $unVisiteur) {
-                            $idvisi = $unVisiteur['visiteur'];
-                            if ($selectedValue == $idvisi) {
-                                ?><option selected value="<?php echo $unVisiteur['visiteur'] ?>"><?php echo $unVisiteur['visiteur'] ?></option>               
-                            <?php } else { ?> <option value="<?php echo $idvisi ?>"><?php echo $idvisi ?></option> <?php
-                            }
-                        }
-                    }
-                    ?>
-                </select>
-            </div>
-            <?php if ($action == 'selectionnerVisiteur' || $action == 'AffichageFicheFraisAndVisiteur') { ?>
+                        ?>
+                    </select>
+                </div>
+
                 <input id="okVisiteur" type="submit" value="Valider" class="btn btn-success" 
                        role="button">
-                   <?php } ?>
+<?php } ?>
 
         </form>
     </div>   
@@ -78,8 +84,9 @@
     <?php
     $idVisiteur = filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING);
     $infoFicheDeFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $_SESSION['date']);
-    $infoFraisForfait=$pdo->getLesFraisForfait($idVisiteur, $_SESSION['date']);
+    $infoFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $_SESSION['date']);
     ?>
+    <h4> Visiteur selectionné : <?php echo $idVisiteur ?></h4>
     <div class="panel panel-info">
         <div class="panel-heading">Fiche</div>
         <table class="table table-bordered table-responsive">
@@ -91,13 +98,11 @@
                 <th>Libelle Etat</th>
             </tr>
             <?php
-            foreach ($infoFicheDeFrais as $infoFiche) {
-                $date = $infoFiche['dateModif'];
-                $nbJustificatifs = $infoFiche['nbJustificatifs'];
-                $montant = $infoFiche['montantValide'];
-                $libelle = $infoFiche['libEtat'];
-                $idEtat = $infoFiche['idEtat'];
-            
+            $date = $infoFicheDeFrais['dateModif'];
+            $nbJustificatifs = $infoFicheDeFrais['nbJustificatifs'];
+            $montant = $infoFicheDeFrais['montantValide'];
+            $libelle = $infoFicheDeFrais['libEtat'];
+            $idEtat = $infoFicheDeFrais['idEtat'];
             ?>
             <tr>
                 <td><?php echo $date ?></td>
@@ -106,7 +111,7 @@
                 <td><?php echo $idEtat ?></td>
                 <td><?php echo $libelle ?></td>
             </tr>
-            <?php }?>
+
         </table>
         <div class="panel-heading">Eléments forfaistisés</div>
         <table class="table table-bordered table-responsive">
@@ -115,21 +120,27 @@
                 <th>IDLibelle</th>
                 <th>Quantités</th>
                 <th>Prix</th>
-                <th>Total</th>
+                <th>Total</th>              
             </tr>
-            <?php foreach($infoFraisForfait as $frais){
-                $idLibelle=$frais['idfrais'];
-                $libelleFrais=$frais['libelle'];
-                $quantite=$frais['quantite'];
-                $prix=$frais['prix'];
-             ?>
-            <tr>
-                <td><?php echo $libelleFrais ?></td>
-                <td><?php echo $idLibelle ?></td>
-                <td><?php echo $quantite ?></td>
-                <td><?php echo $prix ?></td>
-            </tr>
-            <?php } ?>
+            <?php
+            $total = 0;
+            foreach ($infoFraisForfait as $frais) {
+                $idLibelle = $frais['idfrais'];
+                $libelleFrais = $frais['libelle'];
+                $quantite = $frais['quantite'];
+                $prix = $frais['prix'];
+                $total += $prix * $quantite;
+                ?>
+                <tr>
+                    <td><?php echo $libelleFrais ?></td>
+                    <td><?php echo $idLibelle ?></td>
+                    <td><?php echo $quantite ?></td>
+                    <td><?php echo $prix ?></td>
+                    <td><?php echo $prix * $quantite ?></td>                   
+                </tr>                            
+    <?php } ?>
         </table>
+        <div class="panel-heading">Coût des frais forfaits : <?php echo $total . '€' ?></div>
     </div>
-<?php } ?>
+    <?php
+}?>
